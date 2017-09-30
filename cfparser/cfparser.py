f619d227ea70dcceead2169fa922c6795bdf4246
mratkovic <marko.ratkovic93@gmail.com>
2017-09-30 20:36:52 +0200

    Added dumping tests to files and creating file structure for contest

--- cfparser/cfparser.py
+++ cfparser/cfparser.py
@@ -1,8 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
+import os
 
 CONTEST_URL_SCHEME = 'http://codeforces.com/contest/{contest}'
 PROBLEM_URL_SCHEME = 'http://codeforces.com/contest/{contest}/problem/{problem}'
+
+TESTS_SUBDIR = 'tests'
+
+IN_FILE_PATTERN = 'test.in.{id}'
+OUT_FILE_PATTERN = 'test.out.{id}'
 
 
 def parse_contest_for_problem_list(contest_id):
@@ -40,3 +46,30 @@ def parse_problem(contest_id, problem_id):
     ins = get_sample_data(tests.findAll('div', 'input'))
     outs = get_sample_data(tests.findAll('div', 'output'))
     return zip(ins, outs)
+
+
+def dump_tests_to_file(contest, problem, tests, root='./'):
+    tests_path = os.path.join(root, str(contest), problem, TESTS_SUBDIR)
+    os.makedirs(tests_path, exist_ok=True)
+
+    def dump_to_file(data, file_path):
+        with open(file_path, 'w') as fout:
+            fout.write(data)
+
+    for i, (in_data, out_data) in enumerate(tests):
+        in_path = os.path.join(tests_path, IN_FILE_PATTERN.format(id=i))
+        out_path = os.path.join(tests_path, OUT_FILE_PATTERN.format(id=i))
+
+        dump_to_file(in_data, in_path)
+        dump_to_file(out_data, out_path)
+
+
+def download_cf_contest(contest, root_path='./'):
+    problem_list = parse_contest_for_problem_list(contest)
+
+    for problem in problem_list:
+        tests_data = parse_problem(contest, problem)
+        dump_tests_to_file(contest, problem, tests_data, root_path)
+
+
+
